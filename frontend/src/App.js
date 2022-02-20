61593b18b81393ccac2130dc61cb8b4c100eedb2
LuizaI <92949986+LuizaI@users.noreply.github.com>
2022-02-20 22:37:12 +0200

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Components
+import Navbar from "./components/Navbar";
+import SideDrawer from "./components/SideDrawer";
+import Backdrop from "./components/Backdrop";
+
+// Screens
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
+
+function App() {
+  const [sideToggle, setSideToggle] = useState(false);
+  return (
+    <Router>
+      <Navbar click={() => setSideToggle(true)} />
+      <SideDrawer show={sideToggle} click={() => setSideToggle(false)} />
+      <Backdrop show={sideToggle} click={() => setSideToggle(false)} />
+      <main className="app">
+        <Routes>
+          <Route exact path="/" element={<HomeScreen/>} />
+          <Route exact path="/product/:id" element={<ProductScreen/>} />
+          <Route exact path="/cart" element={<CartScreen/>} />
+        </Routes>
+      </main>
+    </Router>
+  );
+}
+
+export default App;
